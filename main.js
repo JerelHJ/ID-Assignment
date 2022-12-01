b7a0a712c1cacef67fcd8e633593d1b2cce97ed5
JerelHJ <115962731+JerelHJ@users.noreply.github.com>
2022-12-02 01:35:35 +0800

    Scrollreveal animation

--- main.js
+++ main.js
@@ -34,3 +34,22 @@ function scrollHeader(){
 
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL REVEAL ===============*/
+const sr = ScrollReveal({
+    origin:'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //rest: true
+})
+
+sr.reveal('.home_title, .popular_container, .features_img, .featured_filter')
+sr.reveal('.home_subtitle',{delay: 500})
+sr.reveal('.home_electric',{delay: 500})
+sr.reveal('.home_car-data',{delay: 900, interval:100, origin: 'bottom'})
+sr.reveal('.home_button',{delay: 1000,  origin: 'bottom'})
+sr.reveal('.about_group, .offer_data',{ origin: 'left'})
+sr.reveal('.about_data, .offer_img',{ origin: 'right'})
+sr.reveal('.features_card', {interval:300})
+sr.reveal('.featured_card, .footer_content', {interval:100})
